368175afa15a4eac9bb3922a4824cdf2b11116f5
woodwardtw <twwoodward@me.com>
2022-05-11 12:30:59 -0400

    trying to index articles, fix blog switch

--- index.php
+++ index.php
@@ -124,6 +124,19 @@ function rlv_restore_blog() {
     }
 }
 
+
+add_filter( 'the_content', 'rlv_restore_shortcode', 1 );
+ 
+function rlv_restore_shortcode( $content ) {
+     if ( is_search() ) {
+        //rlv_restore_blog();
+        switch_to_blog(1);
+        return $content;
+    } else {
+        return $content;
+    }
+}
+
 //LOGGER -- like frogger but more useful
 
 if ( ! function_exists('write_log')) {
@@ -137,3 +150,69 @@ if ( ! function_exists('write_log')) {
 }
 
   //print("<pre>".print_r($a,true)."</pre>");
+
+//article custom post type
+
+// Register Custom Post Type article
+// Post Type Key: article
+
+function create_article_cpt() {
+
+  $labels = array(
+    'name' => __( 'Articles', 'Post Type General Name', 'textdomain' ),
+    'singular_name' => __( 'Article', 'Post Type Singular Name', 'textdomain' ),
+    'menu_name' => __( 'Article', 'textdomain' ),
+    'name_admin_bar' => __( 'Article', 'textdomain' ),
+    'archives' => __( 'Article Archives', 'textdomain' ),
+    'attributes' => __( 'Article Attributes', 'textdomain' ),
+    'parent_item_colon' => __( 'Article:', 'textdomain' ),
+    'all_items' => __( 'All Articles', 'textdomain' ),
+    'add_new_item' => __( 'Add New Article', 'textdomain' ),
+    'add_new' => __( 'Add New', 'textdomain' ),
+    'new_item' => __( 'New Article', 'textdomain' ),
+    'edit_item' => __( 'Edit Article', 'textdomain' ),
+    'update_item' => __( 'Update Article', 'textdomain' ),
+    'view_item' => __( 'View Article', 'textdomain' ),
+    'view_items' => __( 'View Articles', 'textdomain' ),
+    'search_items' => __( 'Search Articles', 'textdomain' ),
+    'not_found' => __( 'Not found', 'textdomain' ),
+    'not_found_in_trash' => __( 'Not found in Trash', 'textdomain' ),
+    'featured_image' => __( 'Featured Image', 'textdomain' ),
+    'set_featured_image' => __( 'Set featured image', 'textdomain' ),
+    'remove_featured_image' => __( 'Remove featured image', 'textdomain' ),
+    'use_featured_image' => __( 'Use as featured image', 'textdomain' ),
+    'insert_into_item' => __( 'Insert into article', 'textdomain' ),
+    'uploaded_to_this_item' => __( 'Uploaded to this article', 'textdomain' ),
+    'items_list' => __( 'Article list', 'textdomain' ),
+    'items_list_navigation' => __( 'Article list navigation', 'textdomain' ),
+    'filter_items_list' => __( 'Filter Article list', 'textdomain' ),
+  );
+  $args = array(
+    'label' => __( 'article', 'textdomain' ),
+    'description' => __( '', 'textdomain' ),
+    'labels' => $labels,
+    'menu_icon' => '',
+    'supports' => array('title', 'editor', 'revisions', 'author', 'trackbacks', 'custom-fields', 'thumbnail',),
+    'taxonomies' => array('category', 'post_tag'),
+    'public' => true,
+    'show_ui' => false,
+    'show_in_menu' => false,
+    'menu_position' => 5,
+    'show_in_admin_bar' => false,
+    'show_in_nav_menus' => false,
+    'can_export' => true,
+    'has_archive' => true,
+    'hierarchical' => false,
+    'exclude_from_search' => false,
+    'show_in_rest' => true,
+    'publicly_queryable' => true,
+    'capability_type' => 'post',
+    'menu_icon' => 'dashicons-universal-access-alt',
+  );
+  register_post_type( 'article', $args );
+  
+  // flush rewrite rules because we changed the permalink structure
+  global $wp_rewrite;
+  $wp_rewrite->flush_rules();
+}
+add_action( 'init', 'create_article_cpt', 0 );
