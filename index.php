7e610407be4f5443e0c77e7b76ba786523bef7b8
woodwardtw <twwoodward@me.com>
2023-02-22 08:01:35 -0500

    issue

--- index.php
+++ index.php
@@ -293,10 +293,10 @@ add_filter('get_the_guid', 'fix_detox_links');
 
 //LIMIT EMAILS TO MIDDLEBURY DOMAIN FOR CATALOG COURSE REQUEST
 
-new GW_Email_Domain_Validator( array(
-    'form_id'            => 15,
-    'field_id'           => 3,
-    'domains'            => array( 'middlebury.edu' ),
-    'validation_message' => __( 'Sorry. You have to use a Middlebury email domain.' ),
-    'mode'               => 'limit',
-) );
+// new GW_Email_Domain_Validator( array(
+//     'form_id'            => 15,
+//     'field_id'           => 3,
+//     'domains'            => array( 'middlebury.edu' ),
+//     'validation_message' => __( 'Sorry. You have to use a Middlebury email domain.' ),
+//     'mode'               => 'limit',
+// ) );
